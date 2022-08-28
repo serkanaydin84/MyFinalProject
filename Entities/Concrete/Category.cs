5035dc4d993e2e0c9145b8f889e23a8b451d0d41
Serkan Aydın <aydinserkan2004@gmail.com>
2022-08-28 14:09:41 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category : IEntity
+    {
+        // Burada yani Concrete klasöründeki class lar DB deki bir tabloya karşılık geliyor
+        // O yüzden Çıplak Class kalmaması gerekiyor.
+        // Yani her class ı bir interface vb. referans tipine bağlamak gerekiyor
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
